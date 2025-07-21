583908cc34c8d8393b6d8b5aad3ff5671f8eee74
Marco Gosselink <marco@famosselink.nl>
2025-07-21 19:31:21 +0200

    Removed number entities

--- custom_components/weerplaza/config_flow.py
+++ custom_components/weerplaza/config_flow.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import logging
 from typing import Any
+import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlowResult
@@ -23,8 +24,12 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
+        STEP_USER_DATA_SCHEMA = vol.Schema({})
+
         if user_input is None:
-            return self.async_show_form(step_id="user")
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
 
         await self.async_set_unique_id(DOMAIN)
         self._abort_if_unique_id_configured()
