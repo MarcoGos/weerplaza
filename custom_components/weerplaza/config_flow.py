f0d464357a662c6553b0bf77b33329d73c0d04e7
Marco Gosselink <marco@famosselink.nl>
2025-07-21 17:02:19 +0200

    Fixed config flow

--- custom_components/weerplaza/config_flow.py
+++ custom_components/weerplaza/config_flow.py
@@ -4,16 +4,12 @@ from __future__ import annotations
 
 import logging
 from typing import Any
-import voluptuous as vol
 
 from homeassistant import config_entries
-
-# from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.exceptions import HomeAssistantError
-# from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN
+from .const import DOMAIN, NAME
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,21 +23,13 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
-        STEP_USER_DATA_SCHEMA = vol.Schema(
-            {
-                vol.Required("name"): str,  # type: ignore
-            }
-        )
-
         if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
-            )
+            return self.async_show_form(step_id="user")
 
         await self.async_set_unique_id(DOMAIN)
         self._abort_if_unique_id_configured()
 
-        return self.async_create_entry(title="Weer Plaza", data=user_input)
+        return self.async_create_entry(title=NAME, data=user_input)
 
 
 class CannotConnect(HomeAssistantError):
